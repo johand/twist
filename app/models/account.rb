b15aead23ab238f719156c0da1a5a807a4a9e6f5
Johan <johand@openmailbox.org>
2020-06-13 00:19:34 -0400

    Accounts are now linked to owners

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Account < ApplicationRecord
+  belongs_to :owner, class_name: 'User'
+  accepts_nested_attributes_for :owner
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_233314) do
+ActiveRecord::Schema.define(version: 2020_06_13_024556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_06_12_233314) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "owner_id"
   end
 
   create_table "books", force: :cascade do |t|
