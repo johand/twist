e0bf68038fb9b35388c3906a90e3580dafec9651
Johan <johand@openmailbox.org>
2020-07-12 23:09:12 -0400

    Allow users to switch between their accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :memberships
+  has_many :accounts, through: :memberships
+
+  def owned_accounts
+    Account.where(owner: self)
+  end
+
+  def all_accounts
+    owned_accounts + accounts
+  end
 end
